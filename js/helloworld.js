74d315e879ceb28fc9b6536a20550f8127351530
ruiteru <fanta.good0052@gmail.com>
2014-04-24 12:34:41 +0900

    んんん。

--- js/helloworld.js
+++ js/helloworld.js
@@ -5,3 +5,11 @@ var helloWorld = function(){
 
 var showHelloWorldButton = document.getElementById("showHelloWorld");
 showHelloWorldButton.addEventListener("click", helloWorld);
+
+var helloWorld2 = function(){
+	var message = document.getElementById("helloWorld2");
+	message.setAttribute("class", "");
+};
+
+var showHelloWorld2Button = document.getElementById("showHelloWorld2");
+showHelloWorld2Button.addEventListener("click", helloWorld2);
